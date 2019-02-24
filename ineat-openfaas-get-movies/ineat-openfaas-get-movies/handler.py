82964a57320776cfa71b152f2badfe5712fdaadb
uendelig <mathias.deracc@gmail.com>
2019-02-24 11:09:01 +0100

    feat(movies): add movies search function

--- ineat-openfaas-get-movies/ineat-openfaas-get-movies/handler.py
+++ ineat-openfaas-get-movies/ineat-openfaas-get-movies/handler.py
@@ -4,7 +4,7 @@ import json
 
 def handle(req):
     # Connexion a la base mongo
-    connect('moviesDB', host='mongodb://192.168.94.247:27017', alias='default')
+    connect('moviesDB', host='mongodb://192.168.1.19:27017', alias='default')
 
     # Extraction de la requête passée en paramètre lors de l'appel
     query = json.loads(req)
