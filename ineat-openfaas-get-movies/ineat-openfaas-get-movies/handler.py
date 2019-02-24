ef2228d9bed9bf854f699fd92ae85f76c9309d9a
uendelig <mathias.deracc@gmail.com>
2019-02-24 17:14:14 +0100

    feat(handler): update get movies handler to get query parameter

--- ineat-openfaas-get-movies/ineat-openfaas-get-movies/handler.py
+++ ineat-openfaas-get-movies/ineat-openfaas-get-movies/handler.py
@@ -7,7 +7,7 @@ def handle(req):
     connect('moviesDB', host='mongodb://192.168.1.19:27017', alias='default')
 
     # Extraction de la requête passée en paramètre lors de l'appel
-    query = json.loads(req)
+    query = json.loads(req)["query"]
     
     # Exécution de la requête
     result = schema.execute(query)
